222e95b5fa71d950a3c153ebce3b92e4ac2ac998
chanawh <thechanwh@gmail.com>
2023-08-03 03:31:20 +0000

    Modified test_todo_list.py

--- test_todo_list.py
+++ test_todo_list.py
@@ -10,7 +10,7 @@ class TestToDoListApp(unittest.TestCase):
         with patch('builtins.input', side_effect=['Task 1']):
             with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
                 add_task(todo_list)
-                self.assertEqual(todo_list, [{'task': 'Task 1', 'completed': False}])
+                self.assertEqual(todo_list, [{'task': 'Task 2', 'completed': False}])
                 self.assertEqual(mock_stdout.getvalue().strip(), "Task added successfully!")
 
 if __name__ == '__main__':
